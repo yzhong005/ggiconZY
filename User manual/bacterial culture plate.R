4bcbfc9d71824d2b2339d3135c37a8c5aaf76d58
Yang <93303657+yzhong005@users.noreply.github.com>
2024-01-30 16:46:16 +0800

    Update bacterial culture plate.R

--- User manual/bacterial culture plate.R
+++ User manual/bacterial culture plate.R
@@ -21,12 +21,12 @@ disc$drug<-c("TZP","AMC","MEM","CTX","TGC","New") # label the disc according to 
 
 ggplot(agar,aes(x,y))+
   geom_polygon(color="black",fill="grey50",alpha=0.2,linewidth=1)+
-  geom_polygon(aes(x=0.98*x,y=0.98*y),color="black",fill="#ffd200",alpha=0.3,linewidth=1)+
-  geom_polygon(aes(x=0.2*x+disc$x[1],y=0.2*y+disc$y[1]),fill="#ffd200",color="grey85",alpha=0.7)+
-  geom_polygon(aes(x=0.1*x+disc$x[2],y=0.1*y+disc$y[2]),fill="#ffd200",color="grey85",alpha=0.7)+
-  geom_polygon(aes(x=0.25*x+disc$x[3],y=0.25*y+disc$y[3]),fill="#ffd200",color="grey85",alpha=0.7)+
-  geom_polygon(aes(x=0.15*x+disc$x[5],y=0.15*y+disc$y[5]),fill="#ffd200",color="grey85",alpha=0.7)+
-  geom_polygon(aes(x=0.18*x+disc$x[6],y=0.18*y+disc$y[6]),fill="#ffd200",color="grey85",alpha=0.7)+
+  geom_polygon(aes(x=0.98*x,y=0.98*y),color="black",fill="#b24745",alpha=0.3,linewidth=1)+
+  geom_polygon(aes(x=0.2*x+disc$x[1],y=0.2*y+disc$y[1]),fill="#b24745",color="grey85",alpha=0.7)+
+  geom_polygon(aes(x=0.1*x+disc$x[2],y=0.1*y+disc$y[2]),fill="#b24745",color="grey85",alpha=0.7)+
+  geom_polygon(aes(x=0.25*x+disc$x[3],y=0.25*y+disc$y[3]),fill="#b24745",color="grey85",alpha=0.7)+
+  geom_polygon(aes(x=0.15*x+disc$x[5],y=0.15*y+disc$y[5]),fill="#b24745",color="grey85",alpha=0.7)+
+  geom_polygon(aes(x=0.18*x+disc$x[6],y=0.18*y+disc$y[6]),fill="##b24745",color="grey85",alpha=0.7)+
   geom_point(aes(x,y),data=disc,size=8,color="white")+
   geom_text(aes(label=drug),data=disc,size=2,fontface="bold")+
   geom_text(x=0,y=0,label="My isolate ID",size=5,fontface="bold")+
